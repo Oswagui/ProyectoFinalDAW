8ea0ed170bde041bcdf0403206ccd536698dc500
Oswagui <oswjagui@espol.edu.ec>
2019-02-07 01:08:04 -0500

    Adaptar paginas primer parcial

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  #get 'pages/home'
+
+  root 'pages#home'
+  get 'quienes_somos'  => 'pages#quienes_somos'
+  get 'home' => 'pages#home'
+  get 'que_hacemos' => "pages#que_hacemos"
+  get 'proximos_eventos' => "pages#proximos_eventos"
+  get 'contactenos' => "pages#contactenos"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
